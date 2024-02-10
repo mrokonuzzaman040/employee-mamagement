b2fa3b1513fcdf392aa1ae1d495eb30df9a7284f
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-02-10 23:32:32 +0600

    Add @radix-ui/react-slot package and update page component

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import TitleSection from '@/components/landing-page/title-section';
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 const HomePage = () => {
@@ -6,6 +7,29 @@ const HomePage = () => {
         <section>
             <div className="overflow-hidden px-4 sm:sx-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
                 <TitleSection pill='🥰 your WorkSpace is here' title=' All-In-One Collaboration and Productivity Platform' />
+            </div>
+            <div
+                className="bg-white
+          p-[2px]
+          mt-6
+          rounded-xl
+          bg-gradient-to-r
+          from-primary
+          to-brand-primaryBlue
+          sm:w-[300px]
+        "
+            >
+                <Button
+                    variant="btn-secondary"
+                    className=" w-full
+            rounded-[10px]
+            p-6
+            text-2xl
+            bg-background
+          "
+                >
+                    Get Cypress Free
+                </Button>
             </div>
         </section>
     );
