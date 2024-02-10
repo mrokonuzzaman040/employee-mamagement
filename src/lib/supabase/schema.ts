3ec0778daacf0de8b58852a603ed1efd9f72b45d
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-02-10 22:14:33 +0600

    Add subscriptions table to schema

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -71,3 +71,49 @@ export const files = pgTable('files', {
             onDelete: 'cascade',
         }),
 });
+
+
+export const subscriptions = pgTable('subscriptions', {
+    id: text('id').primaryKey().notNull(),
+    userId: uuid('user_id').notNull(),
+    status: subscriptionStatus('status'),
+    metadata: jsonb('metadata'),
+    priceId: text('price_id').references(() => prices.id),
+    quantity: integer('quantity'),
+    cancelAtPeriodEnd: boolean('cancel_at_period_end'),
+    created: timestamp('created', { withTimezone: true, mode: 'string' })
+        .default(sql`now()`)
+        .notNull(),
+    currentPeriodStart: timestamp('current_period_start', {
+        withTimezone: true,
+        mode: 'string',
+    })
+        .default(sql`now()`)
+        .notNull(),
+    currentPeriodEnd: timestamp('current_period_end', {
+        withTimezone: true,
+        mode: 'string',
+    })
+        .default(sql`now()`)
+        .notNull(),
+    endedAt: timestamp('ended_at', {
+        withTimezone: true,
+        mode: 'string',
+    }).default(sql`now()`),
+    cancelAt: timestamp('cancel_at', {
+        withTimezone: true,
+        mode: 'string',
+    }).default(sql`now()`),
+    canceledAt: timestamp('canceled_at', {
+        withTimezone: true,
+        mode: 'string',
+    }).default(sql`now()`),
+    trialStart: timestamp('trial_start', {
+        withTimezone: true,
+        mode: 'string',
+    }).default(sql`now()`),
+    trialEnd: timestamp('trial_end', {
+        withTimezone: true,
+        mode: 'string',
+    }).default(sql`now()`),
+});
