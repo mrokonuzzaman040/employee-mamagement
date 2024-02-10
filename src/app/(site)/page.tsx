ac33624e2b9cf05e50b2648a03fee2db74e74dcc
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-02-10 23:36:25 +0600

    Add banner image to homepage

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,6 +1,10 @@
 import TitleSection from '@/components/landing-page/title-section';
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 import React from 'react';
+
+// Imported Imagess
+import Banner from '../../../public/appBanner.png';
 
 const HomePage = () => {
     return (
@@ -8,6 +12,8 @@ const HomePage = () => {
             <div className="overflow-hidden px-4 sm:sx-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
                 <TitleSection pill='🥰 your WorkSpace is here' title=' All-In-One Collaboration and Productivity Platform' />
             </div>
+
+            
             <div
                 className="bg-white
           p-[2px]
@@ -30,6 +36,36 @@ const HomePage = () => {
                 >
                     Get Cypress Free
                 </Button>
+            </div>
+
+            <div
+                className="md:mt-[-90px]
+          sm:w-full
+          w-[750px]
+          flex
+          justify-center
+          items-center
+          mt-[-40px]
+          relative
+          sm:ml-0
+          ml-[-50px]
+        "
+            >
+                <Image
+                    src={Banner}
+                    alt="Application Banner"
+                />
+                <div
+                    className="bottom-0
+            top-[50%]
+            bg-gradient-to-t
+            dark:from-background
+            left-0
+            right-0
+            absolute
+            z-10
+          "
+                ></div>
             </div>
         </section>
     );
