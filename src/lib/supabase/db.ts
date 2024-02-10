0fef11fe9a01af85d32c6dfd1dcd05db586a089e
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-02-10 22:08:52 +0600

    Update imports and add database URL check

--- src/lib/supabase/db.ts
+++ src/lib/supabase/db.ts
@@ -1,15 +1,17 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as dotenv from "dotenv";
-import * as schema from "../../../migrations/schema";
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as dotenv from 'dotenv';
+import * as schema from '../../../migrations/schema';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
 
-dotenv.config({ path: ".env" });
+dotenv.config({ path: '.env' });
 
 if (!process.env.DATABASE_URL) {
-    console.log("🔴 Cannot find database url");
+    console.log('🔴 no database URL');
 }
 
 const client = postgres(process.env.DATABASE_URL as string, { max: 1 });
 const db = drizzle(client, { schema });
+
 
 export default db;
