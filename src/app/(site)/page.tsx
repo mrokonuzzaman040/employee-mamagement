837dd60414a63094221e108b78f3a88fff9f018a
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-02-11 00:37:48 +0600

    Add new dependencies and update existing ones

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,17 +1,23 @@
+import React from 'react';
+import Image from 'next/image';
 import TitleSection from '@/components/landing-page/title-section';
 import { Button } from '@/components/ui/button';
-import Image from 'next/image';
-import React from 'react';
-import { CLIENTS } from '@/lib/constants';
+import CustomCard from '@/components/landing-page/custom-card';
+import { CardContent, CardTitle } from '@/components/ui/card';
+import clsx from 'clsx';
+import { CLIENTS, PRICING_CARDS, PRICING_PLANS } from '@/lib/constants';
 
 // Imported Imagess
 import Banner from '../../../public/appBanner.png';
 import Cal from '../../../public/cal.png';
+import Diamond from '../../../public/icons/diamond.svg';
+import CheckIcon from '../../../public/icons/check.svg';
 
 
 const HomePage = () => {
     return (
         <>
+            {/* Header Section */}
             <section
                 className=" overflow-hidden
         px-4
@@ -47,7 +53,7 @@ const HomePage = () => {
             bg-background
           "
                     >
-                        Get Cypress Free
+                        Get TeamSphere Free
                     </Button>
                 </div>
 
@@ -82,7 +88,7 @@ const HomePage = () => {
                 </div>
 
             </section>
-
+            {/* logo image with btn section */}
             <section className='relative'>
                 <div className="
                 overflow-hidden
@@ -142,7 +148,7 @@ const HomePage = () => {
                     ))}
                 </div>
             </section >
-
+            {/* Features Section */}
             <section
                 className="px-4
         sm:px-6
@@ -188,6 +194,125 @@ const HomePage = () => {
                         alt="Banner"
                         className="rounded-2xl"
                     />
+                </div>
+            </section>
+            {/* Pricing Section */}
+            <section
+                className="mt-20
+        px-4
+        sm:px-6
+      "
+            >
+                <TitleSection
+                    title="The Perfect Plan For You"
+                    subheading="Experience all the benefits of our platform. Select a plan that suits your needs and take your productivity to new heights."
+                    pill="Pricing"
+                />
+                <div
+                    className="flex 
+        flex-col-reverse
+        sm:flex-row
+        gap-4
+        justify-center
+        sm:items-stretch
+        items-center
+        mt-10
+        "
+                >
+                    {PRICING_CARDS.map((card) => (
+                        <CustomCard
+                            key={card.planType}
+                            className={clsx(
+                                'w-[300px] rounded-2xl dark:bg-black/40 background-blur-3xl relative',
+                                {
+                                    'border-brand-primaryPurple/70':
+                                        card.planType === PRICING_PLANS.proplan,
+                                }
+                            )}
+                            cardHeader={
+                                <CardTitle
+                                    className="text-2xl
+                  font-semibold
+              "
+                                >
+                                    {card.planType === PRICING_PLANS.proplan && (
+                                        <>
+                                            <div
+                                                className="hidden dark:block w-full blur-[120px] rounded-full h-32
+                        absolute
+                        bg-brand-primaryPurple/80
+                        -z-10
+                        top-0
+                      "
+                                            />
+                                            <Image
+                                                src={Diamond}
+                                                alt="Pro Plan Icon"
+                                                className="absolute top-6 right-6"
+                                            />
+                                        </>
+                                    )}
+                                    {card.planType}
+                                </CardTitle>
+                            }
+                            cardContent={
+                                <CardContent key={card.planType} className="p-0">
+                                    <span
+                                        className="font-normal 
+                    text-2xl
+                "
+                                    >
+                                        ${card.price}
+                                    </span>
+                                    {+card.price > 0 ? (
+                                        <span className="dark:text-washed-purple-800 ml-1">
+                                            /mo
+                                        </span>
+                                    ) : (
+                                        ''
+                                    )}
+                                    <p className="dark:text-washed-purple-800">
+                                        {card.description}
+                                    </p>
+                                    <Button
+                                        variant="btn-primary"
+                                        className="whitespace-nowrap w-full mt-4"
+                                    >
+                                        {card.planType === PRICING_PLANS.proplan
+                                            ? 'Go Pro'
+                                            : 'Get Started'}
+                                    </Button>
+                                </CardContent>
+                            }
+                            cardFooter={
+                                <ul
+                                    className="font-normal
+                  flex
+                  mb-2
+                  flex-col
+                  gap-4
+                "
+                                >
+                                    <small>{card.highlightFeature}</small>
+                                    {card.freatures.map((feature) => (
+                                        <li
+                                            key={feature}
+                                            className="flex
+                      items-center
+                      gap-2
+                    "
+                                        >
+                                            <Image
+                                                src={CheckIcon}
+                                                alt="Check Icon"
+                                            />
+                                            {feature}
+                                        </li>
+                                    ))}
+                                </ul>
+                            }
+                        />
+                    ))}
                 </div>
             </section>
         </>
