ae10e8e9dffbc6c69a721189e83fff39f0bb8530
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-02-12 02:21:25 +0600

    Refactor supabase queries and add createWorkspace function

--- src/lib/supabase/queries.ts
+++ src/lib/supabase/queries.ts
@@ -1,22 +1,31 @@
-'use server';
-import { validate } from 'uuid';
-import { files, folders, users, workspaces } from '../../../migrations/schema';
-import db from './db';
-import { File, Folder, Subscription, User, workspace } from './supabase.types';
-import { and, eq, ilike, notExists } from 'drizzle-orm';
-import { collaborators } from './schema';
-import { revalidatePath } from 'next/cache';
+"use server";
+import { validate } from "uuid";
+import { files, folders, users, workspaces } from "../../../migrations/schema";
+import db from "./db";
+import { File, Folder, Subscription, User, workspace } from "./supabase.types";
+import { and, eq, ilike, notExists } from "drizzle-orm";
+import { collaborators } from "./schema";
+import { revalidatePath } from "next/cache";
+
+export const createWorkspace = async (workspace: workspace) => {
+  try {
+    const response = await db.insert(workspaces).values(workspace);
+    return { data: null, error: null };
+  } catch (error) {
+    console.log(error);
+    return { data: null, error: "Error" };
+  }
+};
 
 export const getUserSubscriptionStatus = async (userId: string) => {
-    try {
-      const data = await db.query.subscriptions.findFirst({
-        where: (s, { eq }) => eq(s.userId, userId),
-      });
-      if (data) return { data: data as Subscription, error: null };
-      else return { data: null, error: null };
-    } catch (error) {
-      console.log(error);
-      return { data: null, error: `Error` };
-    }
-  };
-  
+  try {
+    const data = await db.query.subscriptions.findFirst({
+      where: (s, { eq }) => eq(s.userId, userId),
+    });
+    if (data) return { data: data as Subscription, error: null };
+    else return { data: null, error: null };
+  } catch (error) {
+    console.log(error);
+    return { data: null, error: `Error` };
+  }
+};
