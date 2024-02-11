71674b98432cda993f9c0741500c58414aea87df
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-02-12 02:17:11 +0600

    Update function to handle edge cases

---
+++ src/lib/supabase/queries.ts
@@ -0,0 +1,22 @@
+'use server';
+import { validate } from 'uuid';
+import { files, folders, users, workspaces } from '../../../migrations/schema';
+import db from './db';
+import { File, Folder, Subscription, User, workspace } from './supabase.types';
+import { and, eq, ilike, notExists } from 'drizzle-orm';
+import { collaborators } from './schema';
+import { revalidatePath } from 'next/cache';
+
+export const getUserSubscriptionStatus = async (userId: string) => {
+    try {
+      const data = await db.query.subscriptions.findFirst({
+        where: (s, { eq }) => eq(s.userId, userId),
+      });
+      if (data) return { data: data as Subscription, error: null };
+      else return { data: null, error: null };
+    } catch (error) {
+      console.log(error);
+      return { data: null, error: `Error` };
+    }
+  };
+  
