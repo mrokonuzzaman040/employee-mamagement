b8f8fde081952fda6d7ed0ef10da74521807da8b
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-02-12 00:06:03 +0600

    Add client-side routing and error handling to login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+'use client';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+
 
 const page = () => {
+    const router = useRouter();
+    const [submitError, setSubmitError] = useState('');
     return (
         <div>
-
+            <h1>Login</h1>
         </div>
     );
 };
